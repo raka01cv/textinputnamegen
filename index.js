aa507beff87837e3ec1d05ef6ca2750ec04155c1
rahkanportfolio <rahul.kanjilimadom@cellavision.com>
2024-02-29 16:02:30 +0100

    Textinput

---
+++ index.js
@@ -0,0 +1,49 @@
+const imageContainer = document.getElementById('imageContainer');
+
+function generateImages() {
+    imageContainer.innerHTML = ''; // Clear previous images
+    nameInput = document.getElementById('nameInput').value;
+    const nameArray = nameInput.split('');
+    nameArray.forEach(async (input) => {
+        const letter = input.toUpperCase();
+        if (letter) {
+            const imageUrl = `images/${letter}.png`; // Assuming images are named with uppercase letters
+            const imgElement = document.createElement('img');
+            imgElement.src = imageUrl;
+            imageContainer.appendChild(imgElement);
+        }
+    });
+}
+
+function captureScreenshot() {
+    const container = document.getElementById('imageContainer');
+
+    html2canvas(container).then(canvas => {
+        const imageUrl = canvas.toDataURL();
+
+        if (imageUrl) {
+            // Construct filename based on input values
+            let filename = 'CellaVision_';
+            inputLetters.forEach(input => {
+                filename += input.value.toLowerCase();
+            });
+            filename += '.png';
+
+            // Create a temporary link element to trigger download
+            const link = document.createElement('a');
+            link.href = imageUrl;
+            link.download = filename;
+
+            // Trigger download
+            link.click();
+        } else {
+            console.error('Image URL is empty.');
+        }
+    }).catch(error => {
+        console.error('An error occurred while capturing the screenshot:', error);
+    });
+}
+
+
+
+
